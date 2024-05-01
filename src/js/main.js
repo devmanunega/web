5eb4d2bcffd9accefc2c25b97db0055d3af1011b
devmanunega <devmanuena@gmail.com>
2024-05-01 12:41:47 -0500

    Sticky navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 // Archivo javascript para la funcionalidad requerida
+
+window.onscroll = function() {
+  stickyNav();
+}
+
+const navbar = document.getElementById('navbar');
+const sticky = navbar.offsetTop;
+
+function stickyNav() {
+  window.scrollY > sticky ?
+    navbar.classList.add('sticky-top','nav-sticky-focus') : navbar.classList.remove('sticky-top', 'nav-sticky-focus');
+}
